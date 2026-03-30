b02c4524681f036bfa679b3683d61313dc0bf1e3
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-03-30 23:41:39 +0530

    Time: 4 ms (66.54%) | Memory: 10 MB (93.98%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = getValue(s[i]);
+            int next = (i + 1 < s.length()) ? getValue(s[i + 1]) : 0;
+
+            if (curr < next)
+                sum -= curr;
+            else
+                sum += curr;
+        }
+
+        return sum;
+    }
+
+private:
+    int getValue(char ch) {
+        if (ch == 'I') return 1;
+        if (ch == 'V') return 5;
+        if (ch == 'X') return 10;
+        if (ch == 'L') return 50;
+        if (ch == 'C') return 100;
+        if (ch == 'D') return 500;
+        if (ch == 'M') return 1000;
+        return 0;
+    }
+};
